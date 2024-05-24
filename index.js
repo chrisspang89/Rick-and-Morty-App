ee5d3635398ba1cf66d0640dedeffcb4b7dddf29
Sabeen Javaid <khannssabeen@gmail.com>
2024-05-24 14:59:38 +0200

    add fetching data task

--- index.js
+++ index.js
@@ -15,16 +15,41 @@ const maxPage = 1;
 const page = 1;
 const searchQuery = "";
 
-cardContainer.append(CharacterCard());
-
+// cardContainer.append(CharacterCard());
+cardContainer.innerHTML = "";
 async function fetchCharacters() {
   const response = await fetch("https://rickandmortyapi.com/api/character");
   const data = await response.json();
   console.log(data);
+  // Creates an array [1, 2, 3, ..., 20]
+  // let htmlCards = new Array(20);
+  // for (let i = 0; i < htmlCards.length; i++) {
+  //   if (data.results[i] !== undefined) {
+  //     htmlCards[i] = {
+  //       title: data.results[i].name,
+  //       status: data.results[i].status,
+  //       image: data.results[i].image,
+  //       type: data.results[i].type,
+  //       occur: data.results[i].episode.length,
+  //     };
+  //   } else {
+  //     htmlCards[i] = {
+  //       title: null,
+  //       status: null,
+  //       image: null,
+  //       type: null,
+  //       occur: null,
+  //     };
+  //   }
+  //   cardContainer.append(CharacterCard(htmlCards[i]));
+  // }
 
-  data.forEach((character) => {});
+  // console.log(htmlCards);
 
-  return data;
+  data.results.forEach((element) => {
+    cardContainer.append(CharacterCard(element));
+    console.log(element);
+  });
 }
 
 fetchCharacters();
